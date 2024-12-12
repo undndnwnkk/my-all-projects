b5aef4453d8920ade954be67bc1a8329807cb03e
undndnwnkk <bykov.lev74@gmail.com>
2024-12-12 12:06:00 +0300

    added function add

--- python/fridge/main.py
+++ python/fridge/main.py
@@ -1,5 +1,20 @@
+from datetime import datetime, date
+from decimal import Decimal
+
 def add(items, title, amount, expiration_date=None):
-    ...
+    new_date = datetime.strptime(expiration_date ,'%Y-%m-%d')
+    
+    
+    if title in items:
+        items[title].append({
+            'amount': Decimal(str(amount)),
+            'expiration_date': datetime.date(new_date)
+        })
+    else:
+        items[title] = {
+            'amount': Decimal(str(amount)),
+            'expiration_date': datetime.date(new_date)
+        }
 
 
 def add_by_note(items, note):
@@ -19,10 +34,8 @@ def expire(items, in_advance_days=0):
     
 goods = {
     'Пельмени Универсальные': [
-        # Первая партия продукта 'Пельмени Универсальные':
-        {'amount': Decimal('0.5'), 'expiration_date': datetime.date(2023, 7, 15)},
-        # Вторая партия продукта 'Пельмени Универсальные':
-        {'amount': Decimal('2'), 'expiration_date': datetime.date(2023, 8, 1)},
+        {'amount': Decimal('0.5'), 'expiration_date': date(2023, 7, 15)},
+        {'amount': Decimal('2'), 'expiration_date': date(2023, 8, 1)},
     ],
     'Вода': [
         {'amount': Decimal('1.5'), 'expiration_date': None}
