7a43c272687f07c525125753e82dc30f1c0999ae
undndnwnkk <bykov.lev74@gmail.com>
2025-01-10 16:25:33 +0300

    added + and += operators

--- cpp/bigint/solution/bigint.inc.cpp
+++ cpp/bigint/solution/bigint.inc.cpp
@@ -107,7 +107,7 @@ public:
     }
 
     // From vector numbers ints -> string method
-    std::string to_string() {
+    std::string to_string() const {
         std::string result;
         for (const auto &current_number :
              std::vector<unsigned int>(numbers.rbegin(), numbers.rend())) {
@@ -127,6 +127,30 @@ public:
         }
 
         return result;
+    }
+    // "+" and "+=" operators
+    bigint& operator+=(const bigint& other) {
+        unsigned int carry = 0;
+        std::size_t max_size = std::max(numbers.size(), other.numbers.size());
+        numbers.resize(max_size);
+
+        for(std::size_t i = 0; i < max_size || carry != 0; i++) {
+            if (i >= numbers.size()) {
+                numbers.push_back(0);
+            }
+
+            numbers[i] += (i < other.numbers.size() ? other.numbers[i] : 0) + carry;
+            carry = numbers[i] / BASE;
+            numbers[i] %= BASE;
+        }
+
+        remove_leading_zeros();
+        return *this;
+    }
+
+    friend bigint operator+(bigint left_number, const bigint& right_number) {
+        left_number += right_number;
+        return left_number;
     }
 };
 
