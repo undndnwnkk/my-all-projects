683b757e47b98e782f872758f0e171a02c19352a
undndnwnkk <bykov.lev74@gmail.com>
2025-01-11 14:05:49 +0300

    started new project - widgets

--- cpp/widgets/lab03-widgets/solution/run-test-data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-[ "$#" -ge 1 ] || (echo "Expected at least one argument: command to run" && exit 1)
-COMMAND=("$@")
-TIMEOUT=15s  # Careful of Valgrind
-
-timeout -k 0.1s "$TIMEOUT" "${COMMAND[@]}"
